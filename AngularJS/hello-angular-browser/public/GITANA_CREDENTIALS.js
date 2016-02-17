814fdcc618bdc5be62d38e4d6cc8fc1d8fffae97
Scott Davis <scott.davis@cloudcms.com>
2016-02-17 14:42:13 +1100

    Changed theme to blue, added creds for tenant

--- AngularJS/hello-angular-browser/public/GITANA_CREDENTIALS.js
+++ AngularJS/hello-angular-browser/public/GITANA_CREDENTIALS.js
@@ -1,12 +1,12 @@
 // normally credentials would not be stored in a file
 // accessible directly from the browser.
 CONNECTION_CREDENTIALS = {
-    "clientKey": "",
-    "clientSecret": "",
-    "username": "",
-    "password": "",
+    "clientKey": "8896705b-033a-4e4d-8114-babf9626f3ab",
+    "clientSecret": "6Xi9oheNrXPKnLs9N+sGeZLco/30mY5UtlwtsCbEkJ1eZHKb5SUHz5lpflxldErhH19+DPohKzX6ez/uTSs939oVuLWPQVKpdtu342NHcIY=",
+    "username": "40f92f21-6339-4daa-aaf3-4bd0ad682b37",
+    "password": "68iZKcV1/W0sc9RahOHSJ6pc0DnAX/aUCEAP1zBCxtkeIBwyxd4KnX2jhCDnFz+aYhwAmRl728I2nPQWYagd1Er5VlVbz03yRkisQK34O/A=",
     "baseURL": "/proxy",
-    "application": ""
+    "application": "eac9ebc5866004bada94"
 };
 
 REPOSITORY = "";
